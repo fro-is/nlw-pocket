37194e92a4e80730aa855d55a701e6e567ae7644
Thiago Frois <zthiagopf@gmail.com>
2024-09-10 17:05:36 -0300

    feat: add homepage

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,5 +1,26 @@
+import { Plus } from 'lucide-react';
+
+import letsStart from './assets/lets-start.svg';
+import logo from './assets/logo.png';
+
 function App() {
-  return <h1 className="bg-black text-white text-3xl">Hello Worldd!</h1>
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="in.orbit illustration" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal <u>cadastrar um</u> agora
+        mesmo?
+      </p>
+      <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600 transition-colors"
+      >
+        <Plus className="size-4" />
+        <span>Cadastrar Meta</span>
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
